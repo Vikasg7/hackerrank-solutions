984b8ea7a6bf6e475c9f038fe10f3fb9415ddc7d
Vikas <v33vikasgautam@gmail.com>
2021-02-20 03:50:25 +0530

    Added 2

---
+++ euler-project/2-even-fibonacci-numbers.clj
@@ -0,0 +1,33 @@
+(require 
+  '[clojure.string :refer [split join]])
+
+; Generates next fib given the last two even fibs 
+(defn even-fib [[f1 f2]]
+  (let [nxt (-> (* 4 f2) (+ f1))]
+  [f2 nxt]))
+
+(defn even-fibs-upto [l]
+  (->> (iterate even-fib [0 2])
+       (map second)
+       (take-while #(<= % l))))
+
+(defn solve [t & ls]
+  (let [sum #(apply + %)]
+  (map (comp sum even-fibs-upto) ls)))
+
+(defn interact [fn]
+  (let [joinIfSeq #(if (seq? %) (join "\n" %) %)]
+  (->> (fn (slurp *in*))
+       (joinIfSeq)
+       (println))))
+
+(defn prepare [input]
+  (let [words  #(split % #"\s")]
+  (->> (words input)
+       (map read-string))))
+
+(defn main []
+  (let [program #(->> (prepare %) (apply solve))]
+  (interact program)))
+
+(main)

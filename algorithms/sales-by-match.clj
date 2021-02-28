9222f5f8f0a7a417d11a84b581c63e118c1a22e9
Vikas <v33vikasgautam@gmail.com>
2021-03-01 03:46:38 +0530

    added sales-by match

---
+++ algorithms/sales-by-match.clj
@@ -0,0 +1,25 @@
+(require 
+  '[clojure.string :refer [split join]])
+
+(defn solve [n & ar]
+  (let [freqs (->> (frequencies ar) (map val))
+        pairs #(quot % 2)
+        sum   #(apply + %)]
+  (sum (map pairs freqs))))
+
+(defn interact [func]
+  (let [joinIfSeq #(if (seq? %) (join "\n" %) %)]
+  (->> (func (slurp *in*))
+       (joinIfSeq)
+       (println))))
+
+(defn prepare [input]
+  (let [words  #(split % #"\s")]
+  (->> (words input)
+       (map read-string))))
+
+(defn main []
+  (let [program #(->> (prepare %) (apply solve))]
+  (interact program)))
+
+(main)

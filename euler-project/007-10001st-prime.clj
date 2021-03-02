986148f403ac5e63d5d75b750d53ec43b799052f
Vikas <v33vikasgautam@gmail.com>
2021-03-03 00:12:41 +0530

    added euler 7

---
+++ euler-project/007-10001st-prime.clj
@@ -0,0 +1,41 @@
+(require 
+  '[clojure.string :refer [split join]])
+
+(defn prime? 
+  ([n]
+    (let [oddNums (iterate #(+ % 2) 3)]
+    (prime? n (cons 2 oddNums))))
+  ([n [i & is]]
+    (let [q (quot n i)
+          r (mod n i)]
+    (cond (< n 2)       false
+          (zero? r)     false
+          (> (* i i) n) true
+          :else         (recur n is)))))
+
+(def primes 
+  (let [oddNums (iterate #(+ % 2) 3)]
+  (lazy-seq (cons 2 (filter prime? oddNums)))))
+
+(defn nth-prime [n]
+  (nth primes (dec n)))
+
+(defn solve [t & nz]
+  (map nth-prime nz))
+
+(defn interact [func]
+  (let [joinIfSeq #(if (seq? %) (join "\n" %) %)]
+  (->> (func (slurp *in*))
+       (joinIfSeq)
+       (println))))
+
+(defn prepare [input]
+  (let [words  #(split % #"\s")]
+  (->> (words input)
+       (map read-string))))
+
+(defn main []
+  (let [program #(->> (prepare %) (apply solve))]
+  (interact program)))
+
+(main)
